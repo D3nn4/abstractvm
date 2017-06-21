7dee64486a9a0b568683b4a56b57d21000b62829
Denna <marjorie.lv@gmail.com>
2017-06-21 18:12:13 +0100

    lexer and token fixed

---
+++ includes/abstractvm.hpp
@@ -0,0 +1,21 @@
+#ifndef _VM_HPP_
+#define _VM_HPP_
+
+#include "token.hpp"
+#include "factory.hpp"
+#include <map>
+#include <vector>
+
+class AbstractVM
+{
+public:
+
+
+private:
+
+    Factory _factory;
+    Lexer _lexer;
+
+};
+
+#endif //_VM_HPP_
--- main.cpp
+++ main.cpp
@@ -34,12 +34,23 @@ std::cout << "\n\n";
     }
 
     Lexer lexer;
-    Token token = lexer.createToken("push int8(2)");
-    if(token.valueOperand != nullptr){
-        std::cout << "Token with input " << token.input << " has an operand with a value of : " << token.valueOperand->toString() << std::endl;
+    {
+        Token token = lexer.createToken("push int8(2)");
+        if(token.valueOperand != nullptr){
+            std::cout << "Token with input " << token.input << " has an operand with a value of : " << token.valueOperand->toString() << std::endl;
+        }
+        else {
+            std::cout << "Token with input " << token.input << " has an nullptr " << std::endl;
+        }
     }
-    else {
-        std::cout << "Token with input " << token.input << " has an nullptr " << std::endl;
+    {
+        Token token = lexer.createToken("push float(8.12)");
+        if(token.valueOperand != nullptr){
+            std::cout << "Token with input " << token.input << " has an operand with a value of : " << token.valueOperand->toString() << std::endl;
+        }
+        else {
+            std::cout << "Token with input " << token.input << " has an nullptr " << std::endl;
+        }
     }
 
     std::cout << "The End.\n";
